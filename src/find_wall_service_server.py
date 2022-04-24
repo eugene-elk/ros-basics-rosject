bc86dcd85e81b8e22461fa8b670502ff5fbe666b
Evgeny Lositsky <evgenylositskyitmo@gmail.com>
2022-04-24 19:57:46 +0000

    rosject part 2: finding wall

--- src/find_wall_service_server.py
+++ src/find_wall_service_server.py
@@ -23,31 +23,37 @@ def callback_scan(msg):
 
     global value_front
     value_front = msg.ranges[360]
-    print("[scan] Value in front: ", value_front)
+    # print("[scan] Value in front: ", value_front)
+    print("[scan] Minimal position: ", minimum_position)
 
 
 def callback_srv(request):
 
     print("[srv] Call Service Server")
 
-    while abs(minimum_position - 360) > 3:
+    while abs(minimum_position - 360) > 15:
+        print("[srv] Rotate")
         move.linear.x = 0
-        move.angular.z = 0.1
+        move.angular.z = 0.25
         pub.publish(move)
+        rospy.sleep(0.5)
 
     print("[srv] Wall is in front of the robot")
 
     while value_front > 0.3:
-        move.linear.x = 0.1
+        print("[srv] Move forward")
+        move.linear.x = 0.05
         move.angular.z = 0
         pub.publish(move)
+        rospy.sleep(0.5)
 
     print("[srv] Wall is closer than 30cm")
 
-    while abs(minimum_position - 180) > 3:
+    while abs(minimum_position - 180) > 15:
         move.linear.x = 0
         move.angular.z = 0.1
         pub.publish(move)
+        rospy.sleep(0.5)
 
     print("[srv] Wall is on the right side")
 
@@ -62,6 +68,7 @@ def callback_srv(request):
 
 
 rospy.init_node('find_wall_node')
+rate = rospy.Rate(10)
 
 sub = rospy.Subscriber('/scan', LaserScan, callback_scan)
 srv = rospy.Service('/find_wall', FindWall, callback_srv)
